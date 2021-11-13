7fded795e2a12e04733c1cbd3f1796653aa694d5
Sam Aparo <sam.aparo@gmail.com>
2021-11-13 15:56:47 -0500

    brand transitions

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const scrollPage = () => {
-  const pageHeight = window.outerHeight;
-  document.querySelectorAll(".invisible").forEach(invis => {
-    invis.classList.add("fadeIn");
-    if (pageHeight >= invis.getBoundingClientRect().top + 0.1 * pageHeight) {
-      invis.classList.toggle("invisible", false);
-    }
-  });
+	const pageHeight = window.outerHeight;
+	document.querySelectorAll('.invisible').forEach((invis) => {
+		invis.classList.add('fadeIn');
+		if (pageHeight >= invis.getBoundingClientRect().top + 0.1 * pageHeight) {
+			invis.classList.toggle('invisible', false);
+		}
+	});
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("scroll", scrollPage);
-  scrollPage();
+const applyBranding = (brand) => {
+	clearBranding();
+	document.querySelector('body').classList.add(brand);
+};
+
+const clearBranding = () => {
+	document.querySelector('body').classList.remove('mailchimp', 'yikyak', 'razorfish');
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+	window.addEventListener('scroll', scrollPage);
+	scrollPage();
+
+	document.querySelectorAll('[data-brand]').forEach((link) => {
+		link.addEventListener('mouseenter', () => {
+			applyBranding(link.dataset.brand);
+		});
+		link.addEventListener('mouseleave', () => {
+			clearBranding();
+		});
+	});
 });
